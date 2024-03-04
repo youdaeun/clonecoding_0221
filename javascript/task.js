3e51448920e2ab8ed6f9f21e4e8d10e388c14dfb
youdaeun <dbekdms586@gmail.com>
2024-03-04 19:37:39 +0900

    main page- 80%

--- javascript/task.js
+++ javascript/task.js
@@ -22,3 +22,41 @@ document.addEventListener("DOMContentLoaded", function () {
       hiddenContainer.style.display === "none" ? "block" : "none";
   });
 });
+
+const bannerWrap = document.querySelector(".banner-wrap");
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+const images = document.querySelectorAll(".banner-img");
+let currentIndex = 0;
+
+function showControls() {
+  prevButton.style.display = "block";
+  nextButton.style.display = "block";
+}
+
+function hideControls() {
+  prevButton.style.display = "none";
+  nextButton.style.display = "none";
+}
+
+function showImage(index) {
+  const width = images[0].clientWidth;
+  bannerBox.style.transform = `translateX(-${index * width}px)`;
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  showImage(currentIndex);
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage(currentIndex);
+}
+
+bannerWrap.addEventListener("mouseenter", showControls);
+bannerWrap.addEventListener("mouseleave", hideControls);
+prevButton.addEventListener("click", prevSlide);
+nextButton.addEventListener("click", nextSlide);
+
+showImage(currentIndex);
